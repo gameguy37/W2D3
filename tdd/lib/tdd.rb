787667103f53ba8ce9d90bd1cd71f0da15e8c75f
Casey <brownc117@gmail.com>
2019-04-17 16:05:23 -0400

    finished TDD module

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -31,6 +31,92 @@ class Array
         array
     end
 
+    def stock_picker
+        max = 0
+        indicies = []
+        self.each_with_index do |ele1, idx1|
+            self.each_with_index do |ele2, idx2|
+                if idx2 > idx1
+                    if ele2 - ele1 > max #|| max.nil?
+                    max = ele2 - ele1
+                    indicies = [idx1, idx2]
+                    end
+                end
+            end
+        end
+        indicies
+    end
+
 end
 
-print [[1, 2, 3], [4, 5, 6], [7, 8, 9]].my_transpose
+class LargeAtopSmallError < StandardError; end
+class EmptyFromTowerError < StandardError; end
+
+class Towers
+
+    attr_reader :T1, :T2, :T3
+
+    def initialize
+        @T1 = [7,6,5,4,3,2,1]
+        @T2 = []
+        @T3 = []
+    end
+
+    def move(response)
+        from_twr, to_twr = response
+        #debugger
+        case from_twr
+        when 1
+            raise EmptyFromTowerError if self.T1.empty?
+            disc = self.T1.pop
+        when 2
+            raise EmptyFromTowerError if self.T2.empty?
+            disc = self.T2.pop
+        when 3
+            raise EmptyFromTowerError if self.T3.empty?
+            disc = self.T3.pop
+        end
+        
+        case to_twr
+        when 1
+            raise LargeAtopSmallError if self.T1[-1] < disc
+            self.T1.push(disc)
+        when 2
+            raise LargeAtopSmallError if self.T2[-1] < disc
+            self.T2.push(disc)
+        when 3
+            raise LargeAtopSmallError if self.T3[-1] < disc
+            self.T3.push(disc)
+        end
+    end
+
+    def get_move
+        valid = false
+        response = []
+        while !valid
+            puts "Please enter a tower to move from and a tower to move to separated by a comma (e.g., 1,3)"
+            response = gets.chomp.split(",").map { |ele| ele.to_i }
+            valid = true if response.all? { |num| between?(1,3) }
+        end
+        #debugger
+        response
+    end
+
+    def won?
+        if self.T2 == [7,6,5,4,3,2,1] || self.T3 == [7,6,5,4,3,2,1]
+            return true
+        end
+        false
+    end
+
+    def cheat
+        self.T3 = [7,6,5,4,3,2,1]
+    end
+
+    attr_writer :T3
+
+
+end
+    
+# game = Towers.new
+# game.get_move
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -25,17 +25,14 @@ describe "#two_sum" do
     subject(:original) {[-2, 2, 1, -3, 4, 3, -1]}
     let(:new) { original.two_sum }
     it "should return a 2-D array" do
-        #new = original.two_sum
         expect(new.all? { |ele| ele.is_a?(Array) }).to be true
     end
 
     it "should populate with indices whose corresponding elements sum to 0" do
-        #new = original.two_sum
         expect(new.all? { |ele| original[ele[0]] + original[ele[1]] == 0 }).to be true
     end
 
     it "should order pairs 'dic-wise'" do
-        #new = original.two_sum
         expect(new).to eq([[0,1], [2,6], [3,5]])
     end
 
@@ -45,13 +42,44 @@ describe "#my_transpose" do
     subject(:original) {[[1,2,3], [4,5,6], [7,8,9]]}
     let(:new) { original.my_transpose }
     it "should return a 2-D array" do
-        #new = original.my_transpose
         expect(new.all? { |ele| ele.is_a?(Array) }).to be true
     end
 
     it "should convert between row-oriented and column-oriented representations" do
-        #new = original.my_transpose
         expect(new).to eq([[1,4,7], [2,5,8], [3,6,9]])
     end
 end
 
+describe "#stock_picker" do
+    subject(:stocks) {[3.67, 4.23, 2.35, 2.45, 4.36, 5.76, 4.65, 1.34]} #indices 2 and 5
+    let(:new) {stocks.stock_picker}
+    it "should pick the most profitable buy sell date indices combo" do 
+        expect(new).to eq([2,5])
+    end
+    it "sell date should not come before buy date" do
+    expect(new[0] < new[1]).to be true
+    end
+end
+
+describe "Towers_of_Hanoi" do
+    subject(:game) { Towers.new }
+
+    it "should #initialize as an instance of the 'Towers' class" do
+        expect(game).to be_an_instance_of(Towers)
+    end
+
+    it "should check that #move works as expected for legal moves" do
+        game.move([1,2])
+        expect(game.T2).to eq([1])
+    end
+
+    it "should check that invalid #moves raise an error" do
+        game.move([1,2])
+        expect { game.move([1,2]) }.to raise(LargeAtopSmallError)
+    end
+    
+    it "should declare the game #won? when either Tower2 or Tower3 contain all discs in numerical order" do
+        game.cheat
+        expect(game.won?).to be true
+    end
+end
